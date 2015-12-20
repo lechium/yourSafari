83b01e716893d23e222c6d7819bdf94acbd02b56
Kevin Bradley <bile@chronic-dev.com>
2015-12-19 20:34:34 -0700

    added icon and new extz

--- ytTV.safariextension/main.js
+++ ytTV.safariextension/main.js
@@ -1,12 +1,12 @@
+
 (function () {
 
 	safari.application.addEventListener('command', function (e) {
 		if (e.command === 'playyt') {
 			var win = safari.application.activeBrowserWindow;
 			var hn = safari.extension.settings.hostname;
-			//alert(hn)
 			var url = 'http://' + hn + '/playyt=' + win.activeTab.url;
-			//alert(url);
+			
 			var xhr = new XMLHttpRequest();
               xhr.open('GET', url, true);                  
               xhr.send(null);
